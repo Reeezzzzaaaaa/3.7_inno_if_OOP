0447e8097e367eea2de15ae21841c07ebbe60d7f
Alexandr Ryazantsev <ryazantsevai92@gmail.com>
2024-06-16 11:40:30 +0300

    rework commit

--- src/Branches/Task_4/Password.java
+++ src/Branches/Task_4/Password.java
@@ -4,15 +4,18 @@ public class Password {
 
     public static void main(String[] args) {
 
-        String password = "fdvdsv1#";
+        String password = "fdvd1!d@";
         //"(?!=.*[0-9]).{0,}"
         if (password.length() < 8) {
             System.out.println("Пароль не менее 8 символов");
-        } else if (!password.matches("(?=.*[0-9]).{0,}")) {
+        }
+        if (!password.matches("(?=.*[0-9]).{0,}")) {
             System.out.println("Пароль должен содержать минимум 1 цифру");
-        } else if (!password.matches("(?=.*[!@#$%^&*№]).{0,}")) {
+        }
+        if (!password.matches("(?=.*[!@#$%^&*№]).{0,}")) {
             System.out.println("Пароль должен содержать минимум 1 спецсимвол");
-        } else {
+        }
+        else if (password.matches("(?=.*[0-9!@#$%^&*№]).{8,}")) {
             System.out.println("Пароль Принят");
         }
     }
